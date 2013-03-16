676de2fef6941777a0997ffeacafa006061a4626
Thomas Linkin <trlinkin@gmail.com>
2013-03-16 00:10:05 -0400

    Big commit, re-worked most of the logic. This is a big commit, with a huge change in the code. A number of things came about in this commit. 1. Support for multi-OS defaults. 2. Defaults for Redhat and Ubuntu systems. 3. Support for arrays or strings as params. 4. Completely move and re-write the logic that picks default vs user data.
    Item 4 was the biggest of the changes. However, This was done to get
    something... anything working. The previous commit had working defaults
    nicely, but that was it, you could not actually modify. I moved the
    picking logic into the nsswitch class proper due to the way manner in
    which variables move around inherited classes.
    
    Small, sane, incremental commits from here on out, I swear!

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,6 +16,99 @@ class nsswitch (
   $shadow     = undef,
 ) inherits nsswitch::params {
 
+# We need to figure out if the use provided input or we use defaults.
+
+  # Determine the value for Aliases
+  if $aliases {
+    $_aliases = $aliases
+  }else{
+    $_aliases = $nsswitch::params::aliases_default
+  }
+  # Determine the value for 
+  if $automount {
+    $_automount = $automount
+  }else{
+    $_automount= $nsswitch::params::automount_default
+  }
+  # Determine the value for Boot Params
+  if $bootparams {
+    $_bootparams = $bootparams
+  }else{
+    $_bootparams = $nsswitch::params::bootparams_default
+  }
+  # Determine the value for Ethers
+  if $ethers {
+    $_ethers = $ethers
+  }else{
+    $_ethers = $nsswitch::params::ethers_default
+  }
+  # Determine the value for Groups
+  if $group {
+    $_group = $group
+  }else{
+    $_group = $nsswitch::params::group_default
+  }
+  # Determine the value for Hosts
+  if $hosts {
+    $_hosts = $hosts
+  }else{
+    $_hosts = $nsswitch::params::hosts_default
+  }
+  # Determine the value for Netgroup
+  if $netgroup {
+    $_netgroup = $netgroup
+  }else{
+    $_netgroup = $nsswitch::params::netgroup_default
+  }
+  # Determine the value for Netmasks
+  if $netmasks {
+    $_netmasks = $netmasks
+  }else{
+    $_netmasks = $nsswitch::params::netmasks_default
+  }
+  # Determine the value for Networks
+  if $networks {
+    $_networks = $networks
+  }else{
+    $_networks = $nsswitch::params::networks_default
+  }
+  # Determine the value for PASSWD
+  if $passwd  {
+    $_passwd = $passwd
+  }else{
+    $_passwd = $nsswitch::params::passwd_default
+  }
+  # Determine the value for Protocols
+  if $protocols {
+    $_protocols = $protocols
+  }else{
+    $_protocols = $nsswitch::params::protocols_default
+  }
+  # Determine the value for Public Key
+  if $publickey {
+    $_publickey = $publickey
+  }else{
+    $_publickey = $nsswitch::params::publickey_default
+  }
+  # Determine the value for RPC
+  if $rpc {
+    $_rpc = $rpc
+  }else{
+    $_rpc = $nsswitch::params::rpc_default
+  }
+  # Determine the value for Services
+  if $services {
+    $_services = $services
+  }else{
+    $_services = $nsswitch::params::services_default
+  }
+  # Determine the value for Shadow 
+  if $shadow {
+    $_shadow = $shadow
+  }else{
+    $_shadow = $nsswitch::params::shadow_default
+  }
+
   file { $nsswitch::params::nsswitch_path:
     owner   => 'root',
     group   => 'root',
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,81 +1,44 @@
 class nsswitch::params {
 
-  # Just incase anything Non-standard ever goes down
-  $nsswitch_path = '/etc/nsswitch.conf'
+  case $operatingsystem {
+    /CentOS|RedHat|Amazon/: {
+                $nsswitch_path = '/etc/nsswitch.conf'
 
-  # default aliases for RedHat 6.x Families
-  $_aliases = $aliases ? {
-    true    => $aliases,
-    default => [ 'files', 'nisplus' ],
-  }
+                $aliases_default    = ['files','nisplus']
+                $automount_default  = ['files','nisplus']
+                $bootparams_default = ['nisplus [NOTFOUND=return]','files']
+                $ethers_default     = ['files']
+                $group_default      = ['files']
+                $hosts_default      = ['files','dns']
+                $netgroup_default   = ['nisplus']
+                $netmasks_default   = ['files']
+                $networks_default   = ['files']
+                $passwd_default     = ['files']
+                $protocols_default  = ['files']
+                $publickey_default  = ['nisplus']
+                $rpc_default        = ['files']
+                $services_default   = ['files']
+                $shadow_default     = ['files']
+    }
+    'Ubuntu': {
+                $nsswitch_path = '/etc/nsswitch.conf'
 
-  $_automount = $automount ? {
-    true    => $automount,
-    default => ['files','nisplus'],
-  }
-
-  $_bootparams = $bootparams ? {
-    true    => $bootparams,
-    default => ['nisplus [NOTFOUND=return] files'],
-  }
-
-  $_ethers = $ethers ? {
-    true    => $ethers,
-    default => ['files'],
-  }
-
-  $_group = $group ? {
-    true    => $group,
-    default => ['files'],
-  }
-
-  $_hosts = $hosts ? {
-    true    => $hosts,
-    default => ['files','dns'],
-  }
-
-  $_netgroup = $netgroup ? {
-    true    => $netgroup,
-    default => ['nisplus'],
-  }
-
-  $_netmasks = $netmasks ? {
-    true    => $netmasks,
-    default => ['files'],
-  }
-
-  $_networks = $networks ? {
-    true    => $networks,
-    default => ['files'],
-  }
-
-  $_passwd = $passwd ? {
-    true    => $passwd,
-    default => ['files'],
-  }
-
-  $_protocols = $protocols ? {
-    true    => $protocols,
-    default => ['files'],
-  }
-
-  $_publickey = $publickey ? {
-    true    => $publickey,
-    default => ['nisplus'],
-  }
-
-  $_rpc = $rpc ? {
-    true    => $rpc,
-    default => ['files'],
-  }
-
-  $_services = $services ? {
-    true    => $services,
-    default => ['files'],
-  }
-
-  $_shadow = $shadow ? {
-    true    => $shadow,
-    default => ['files'],
+                $aliases_default    = undef
+                $automount_default  = undef
+                $bootparams_default = undef
+                $ethers_default     = ['db','files']
+                $group_default      = ['compat']
+                $hosts_default      = ['files','dns']
+                $netgroup_default   = ['nis']
+                $netmasks_default   = undef
+                $networks_default   = ['files']
+                $passwd_default     = ['compat']
+                $protocols_default  = ['db','files']
+                $publickey_default  = undef
+                $rpc_default        = ['db','files']
+                $services_default   = ['db','files']
+                $shadow_default     = ['compat']
+    }
+    default: { fail("${operatingsystem} is not a supported operating system.") }
   }
 }
