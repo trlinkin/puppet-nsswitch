8bf3b04743903c335915a40ae1e5a0752a466498
Thomas Linkin <trlinkin@gmail.com>
2014-11-07 13:31:12 -0500

    Add initial components of automated testing

---
+++ spec/classes/nsswitch_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'nsswitch', :type => :class do
+  %w{CentOS RedHat Amazon OracleLinux Scientific Fedora SLES Solaris Debian Ubuntu Gentoo}.each do |os|
+    context "when used with default parameter on #{os}" do
+      let(:facts) { {:operatingsystem => os } }
+      it { should compile.with_all_deps }
+      it { should have_resource_count(1) }
+    end
+  end
+
+  context 'when used on an unsupported Operatin System' do
+    let(:facts) { {:operatinsystem => 'Windows' } }
+    it do
+      expect {
+        should compile
+      }.to raise_error(Puppet::Error, /is not a supported operating system\./)
+    end
+  end
+end
