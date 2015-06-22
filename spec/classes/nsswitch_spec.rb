511038a2a35ba74cdf1b8563e85245aa7093f1e2
Thomas Linkin <trlinkin@gmail.com>
2015-06-22 16:45:11 +0200

    Update spec tests to use Rspec 3.x syntax
    Starting in Rspec 3.x, the syntax around one-liners has evolved. The `should`
    syntax, while still available, is no longer monkey patched on Object. It is now
    part of the expectation system technically. While it still works similar to the
    syntax of Rspec's past, it is suggested that the syntax be changed to the more
    informative and descriptive `is_expected.to` syntax.
    
    This commit updated the spec test syntax.

--- spec/classes/nsswitch_spec.rb
+++ spec/classes/nsswitch_spec.rb
@@ -4,15 +4,15 @@ describe 'nsswitch', :type => :class do
   %w{CentOS RedHat Amazon OracleLinux Scientific Fedora SLES Solaris Debian Ubuntu Gentoo}.each do |os|
     context "when used with default parameter on #{os}" do
       let(:facts) { {:operatingsystem => os } }
-      it { should compile.with_all_deps }
-      it { should have_resource_count(1) }
+      it { is_expected.to compile.with_all_deps }
+      it { is_expected.to have_resource_count(1) }
     end
   end
 
   context 'when used on an unsupported Operatin System' do
     let(:facts) { {:operatingsystem => 'Windows' } }
     it do
-      expect raise_error(Puppet::Error, /is not a supported operating system\./)
+      is_expected.to raise_error(Puppet::Error, /is not a supported operating system\./)
     end
   end
 end
